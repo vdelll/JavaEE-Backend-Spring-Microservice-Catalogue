7de374559edcedd0e4453bbbf6062a67704158b6
Val <valentin.delaunay1@gmail.com>
2020-05-22 17:43:05 +0200

    Initialisation du projet

---
+++ src/main/java/fr/vdelll/catservice/CatserviceApplication.java
@@ -0,0 +1,13 @@
+package fr.vdelll.catservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatserviceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CatserviceApplication.class, args);
+    }
+
+}
